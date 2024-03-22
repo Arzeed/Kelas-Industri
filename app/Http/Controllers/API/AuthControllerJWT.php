f4e7acb88a9630a2e650caa2c45f3849c447ef59
Arzeed <zdnajalah@gmail.com>
2024-03-22 11:09:58 +0700

    FCM

--- app/Http/Controllers/API/AuthControllerJWT.php
+++ app/Http/Controllers/API/AuthControllerJWT.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
-
+use App\Http\Controllers\AuthControllerJWT;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Traits\ResponseApi;
