ac202cd369ea3d6828b46150dd1f9f88edff236d
Arzeed <zdnajalah@gmail.com>
2024-03-22 10:25:24 +0700

    todo list api task

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Todo;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::create([
+            "name" => "Malana",
+            "email" => "admin@gmail.com",
+            "password" => bcrypt('12345'),
+        ]);
+        User::factory(9)->create();
+        Todo::factory(20)->create();
         User::factory(20)->create();
 
         // \App\Models\User::factory()->create([
